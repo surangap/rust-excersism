851dcb0575d7c100f169597cfcd099557e42e2e5
surangap <19677661+surangap@users.noreply.github.com>
2024-04-08 22:29:48 +1200

    sol gigasecond

--- exercises/practice/gigasecond/src/lib.rs
+++ exercises/practice/gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
-use time::PrimitiveDateTime as DateTime;
+use time::{Duration, PrimitiveDateTime as DateTime};
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    start.saturating_add(Duration::seconds(10_i64.pow(9)))
+    Duration::DAY.as_sec
 }
